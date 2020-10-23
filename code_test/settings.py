32eaf4cfe066a27be561a25237534aa497b443f3
euan <euan@nanoudigital.com>
2020-10-23 03:32:46 +0200

    Improve Pagination + local static serving

--- code_test/settings.py
+++ code_test/settings.py
@@ -128,5 +128,6 @@ STATICFILES_DIRS = [
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100
+    # 1000 > 100
+    'PAGE_SIZE': 1000  # TODO - might be worth fine-tuning on a view-per-view basis
 }
